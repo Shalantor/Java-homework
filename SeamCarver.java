5468bc25ddb6a20d75e9088156994501acb39ff0
shalantor <gkaraolanis@uth.gr>
2016-04-05 16:17:44 +0300

    Main method now asks for width, height and name of destination file

--- SeamCarver.java
+++ SeamCarver.java
@@ -5,7 +5,7 @@ import java.awt.image.BufferedImage;
 import java.net.*;
 import java.io.*;
 import java.lang.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class SeamCarver{
 
@@ -45,11 +45,14 @@ public class SeamCarver{
     /*Main method*/
     public static void main(String[] args){
 
-        SeamCarver test;
+        SeamCarver seam;
         URL inputLink = null;           //case we get a link
         File inputFile = null;          //case we get a path to a file
         String path = null;
         Scanner input = new Scanner(System.in);
+        int newWidth;
+        int newHeight;
+        String destinationFile;
 
         /*TODO:It is stated in the excercise what we have ask user
         for a valid input if he doesn't enter one when starting the programm*/
@@ -58,7 +61,7 @@ public class SeamCarver{
 
         if(args.length == 0){
             System.out.println("Please enter a valid path to an existing image");
-            System.out.println("or enter a valid url");
+            System.out.print("or enter a valid URL:");
             path = input.nextLine();
         }
         else{
@@ -69,24 +72,50 @@ public class SeamCarver{
         try{
             inputLink = new URL(path);
             try{
-                test = new SeamCarver(inputLink);
+                seam = new SeamCarver(inputLink);
             }
             catch(IOException e){
                 System.out.println("Couldn't open picture from given link");
+                System.exit(0);
             }
          }
         catch(MalformedURLException e){         //if url is malformed, try to interpret
             inputFile = new File(path);         //input as a path to a local file
             try{
-                test = new SeamCarver(inputFile);
+                seam = new SeamCarver(inputFile);
             }
             catch(IOException ex){
                 System.out.println("Couldn't open given file or URL is malformed");
+                System.exit(0);
             }
         }
 
+        /*TODO:optimize WORKAROUND of nextInt() not consuming \n character*/
+        /*Ask for desired dimensions of image to create*/
 
+        try{
+            System.out.print("Please enter desired width:");
+            newWidth = input.nextInt();
+            input.nextLine();                   //consume \n character
+        }
+        catch(InputMismatchException e){
+            System.out.println("This is not a valid integer");
+            System.exit(0);
+        }
 
+        try{
+            System.out.print("Please enter desired height:");
+            newHeight = input.nextInt();
+            input.nextLine();                   //consume \n character
+        }
+        catch(InputMismatchException e){
+            System.out.println("This is not a valid integer");
+            System.exit(0);
+        }
+
+        /*ask for name of file to store the results of calculation*/
+        System.out.print("Please enter the name of destination file (must be a .png file):");
+        destinationFile = input.nextLine();
 
     }
 }
